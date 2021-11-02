53a51577bcdf985344233fdead37376b2be492d2
Thurston Alswager <thurston_alswager@yahoo.com>
2021-11-02 16:03:18 -0500

    Properties

---
+++ Domain/Post.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Domain
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
