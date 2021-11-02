c9498e8220c977bccdcb5287750e863cd895d735
Thurston Alswager <thurston_alswager@yahoo.com>
2021-11-02 17:17:15 -0500

    endpoints

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Application.Posts;
 using Domain;
+using Persistence;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,26 @@ namespace API.Controllers
         
         public PostsController(IMediator mediator) => this.mediator = mediator;
 
+        /// <summary>
+        ///  GET api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
+        [HttpGet]
+        public ActionResult<List<Post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// <param name="id">Post id</param>
+        /// </summary>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
         public async Task<ActionResult<List<Post>>> List()
         {
             return await this.mediator.Send(new List.Query());
